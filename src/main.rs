c80d407f1bb322be1c849ea08fe0c7604ec3c9c7
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-12-11 13:54:26 -0300

    melhorado código do menu

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,30 @@ mod backtracking;
 mod gerador;
 mod programacao_dinamica;
 
-use std::io;
-
 fn main() {
     println!("Escolha o que quer executar:");
     println!("1 - Backtracking");
     println!("2 - Programação Dinâmica");
     println!("0 - Sair");
+
+    use std::io;
+
     loop {
-        let mut input = String::new();
         println!("Digite a opção desejada:");
+        let mut input = String::new();
         if io::stdin().read_line(&mut input).is_err() {
             println!("Erro ao ler entrada");
             continue;
         }
-        if let Ok(x) = input.trim().parse::<u8>() {
-            match x {
-                1 => {
-                    backtracking::backtracking();
-                }
-                2 => {
-                    programacao_dinamica::programacao_dinamica();
-                }
-                0 => {
-                    println!("Saindo...");
-                    break;
-                }
-                _ => {
-                    println!("Entrada inválida");
-                }
+
+        match input.trim().parse::<u8>() {
+            Ok(1) => backtracking::backtracking(),
+            Ok(2) => programacao_dinamica::programacao_dinamica(),
+            Ok(0) => {
+                println!("Saindo...");
+                break;
             }
-        } else {
-            println!("Entrada inválida");
+            _ => println!("Entrada inválida"),
         }
     }
 }
