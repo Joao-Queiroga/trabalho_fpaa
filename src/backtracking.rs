42572265e930b2775e0ffd0287ca5b1dc4169d6a
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-12-12 18:23:25 -0300

    refeita função para fazer o backtracking

--- src/backtracking.rs
+++ src/backtracking.rs
@@ -1,72 +1,52 @@
-use super::gerador::gerador_de_rotas;
-use std::time::{Duration, Instant};
+// Distribui necessariamente todas as rotas, que são os i32 marcando a distancia das rotas, para os caminhões de modo a fazer com que todos os caminhões percorram
+// uma distância igual ou a mais próxima possível usando backtracking com poda, de modo em que
+// todas as rotas sejam designadas a um caminhão mesmo que os caminhões percorram mais de uma rota.
+// O conjunto de rotas é passado como &[i32] em que o valor de cada i32 é a distancia da rota
+pub fn backtracking(rotas: &[i32], n_caminhoes: usize) -> Vec<Vec<i32>> {
+    let mut melhor_distribuicao = vec![vec![]; n_caminhoes];
+    let mut melhor_distancia = 0;
 
-fn distribuir_rotas(
-    rotas: &[i32],
-    num_caminhoes: usize,
-    caminhao_atual: usize,
-    distribuicao_atual: &mut Vec<Vec<i32>>,
-    melhores_distribuicoes: &mut Vec<Vec<Vec<i32>>>,
-) {
-    if caminhao_atual == num_caminhoes {
-        melhores_distribuicoes.push(distribuicao_atual.to_vec());
-        return;
+    fn backtracking_recursivo(
+        rotas: &[i32],
+        distribuicao_atual: &mut Vec<Vec<i32>>,
+        melhor_distribuicao: &mut Vec<Vec<i32>>,
+        melhor_distancia: &mut i32,
+        caminhao_atual: usize,
+    ) {
+        if rotas.is_empty() {
+            let distancia_total: i32 = distribuicao_atual
+                .iter()
+                .map(|caminhao| caminhao.iter().sum::<i32>())
+                .sum();
+
+            if *melhor_distancia == 0 || distancia_total < *melhor_distancia {
+                *melhor_distancia = distancia_total;
+                *melhor_distribuicao = distribuicao_atual.clone();
+            }
+            return;
+        }
+
+        for i in 0..distribuicao_atual.len() {
+            distribuicao_atual[caminhao_atual].push(rotas[0]);
+            backtracking_recursivo(
+                &rotas[1..],
+                distribuicao_atual,
+                melhor_distribuicao,
+                melhor_distancia,
+                (caminhao_atual + 1) % distribuicao_atual.len(),
+            );
+            distribuicao_atual[caminhao_atual].pop();
+        }
     }
 
-    for (i, &rota) in rotas.iter().enumerate() {
-        distribuicao_atual[caminhao_atual].push(rota);
-
-        distribuir_rotas(
-            &rotas[i + 1..],
-            num_caminhoes,
-            caminhao_atual + 1,
-            distribuicao_atual,
-            melhores_distribuicoes,
-        );
-
-        distribuicao_atual[caminhao_atual].pop();
-    }
-}
-
-pub fn encontrar_melhor_distribuicao(rotas: &[i32], num_caminhoes: usize) -> Vec<Vec<Vec<i32>>> {
-    let mut melhores_distribuicoes = Vec::new();
-    let mut distribuicao_atual = vec![vec![]; num_caminhoes];
-
-    distribuir_rotas(
+    let mut distribuicao_atual = vec![vec![]; n_caminhoes];
+    backtracking_recursivo(
         rotas,
-        num_caminhoes,
-        0,
         &mut distribuicao_atual,
-        &mut melhores_distribuicoes,
+        &mut melhor_distribuicao,
+        &mut melhor_distancia,
+        0,
     );
 
-    melhores_distribuicoes
-}
-
-pub fn testar_tempo_execucao() -> usize {
-    let mut quant_rotas: i32 = 6;
-    let tempo_limite = Duration::from_secs(30);
-
-    loop {
-        let mut tempos_execucao = Vec::new();
-
-        for rotas in gerador_de_rotas(quant_rotas, 10, 0.5) {
-            let inicio = Instant::now();
-            let _melhor_distribuicao = encontrar_melhor_distribuicao(&rotas, 3);
-            let duracao = inicio.elapsed();
-
-            tempos_execucao.push(duracao);
-        }
-
-        let tempo_medio: Duration =
-            tempos_execucao.iter().sum::<Duration>() / tempos_execucao.len() as u32;
-
-        if tempo_medio >= tempo_limite {
-            break;
-        }
-
-        quant_rotas += 1;
-    }
-
-    quant_rotas as usize
+    melhor_distribuicao
 }
