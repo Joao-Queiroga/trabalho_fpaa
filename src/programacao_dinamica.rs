495e20f4b249c916416b1876666f5c0b681906ad
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-12-14 22:22:12 -0300

    corrigido código

--- src/programacao_dinamica.rs
+++ src/programacao_dinamica.rs
@@ -6,9 +6,9 @@ pub fn programacao_dinamica(rotas: &[i32], n_caminhoes: usize) -> Vec<Vec<i32>> 
     let margem = alvo / 10;
     let mut caminhoes: Vec<Vec<i32>> = vec![vec![]; n_caminhoes];
 
-    for caminhao in &mut caminhoes {
+    for caminhao in caminhoes.iter_mut().take(n_caminhoes - 1) {
         let mut pd = vec![vec![false; (alvo + margem + 1) as usize]; n_rotas + 1];
-        let mut escolhas = vec![vec![std::i32::MAX; (alvo + margem + 1) as usize]; n_rotas + 1];
+        let mut escolhas = vec![vec![0; (alvo + margem + 1) as usize]; n_rotas + 1];
 
         pd.iter_mut().for_each(|linha| linha[0] = true);
 
@@ -23,15 +23,15 @@ pub fn programacao_dinamica(rotas: &[i32], n_caminhoes: usize) -> Vec<Vec<i32>> 
             }
         }
 
-        let mut melhor_soma = (alvo..=(alvo + margem))
+        let mut melhor_soma = (alvo..=alvo + margem)
             .find(|&j| pd[n_rotas][j as usize])
             .unwrap_or(0);
 
-        for i in (1..=n_rotas).rev() {
-            if escolhas[i][melhor_soma as usize] != 0 && rotas[i - 1] != 0 {
-                caminhao.push(rotas[i - 1]);
-                melhor_soma -= rotas[i - 1];
-                rotas[i - 1] = 0;
+        for i in (0..n_rotas).rev() {
+            if escolhas[i + 1][melhor_soma as usize] != 0 && rotas[i] != 0 {
+                caminhao.push(rotas[i]);
+                melhor_soma -= rotas[i];
+                rotas[i] = 0;
             }
             if melhor_soma <= 0 {
                 break;
@@ -39,6 +39,6 @@ pub fn programacao_dinamica(rotas: &[i32], n_caminhoes: usize) -> Vec<Vec<i32>> 
         }
     }
 
-    caminhoes[n_caminhoes - 1].extend(rotas.iter().filter(|&rota| *rota != 0));
+    caminhoes[n_caminhoes - 1].extend(rotas.iter().filter(|&rota| *rota > 0));
     caminhoes
 }
